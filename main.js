106eec26c1f1803043cfa71075d8b511519e1575
Izhakhtml <Izhaklijalem28@gmail.com>
2021-08-05 14:49:06 +0300

    javascript

---
+++ main.js
@@ -0,0 +1,56 @@
+//                 // תרגיל 1
+// var x =Number (prompt("your age"))
+// var y =Number (prompt("your age"))
+// var user = x,y;
+// console.log(x,y);
+// if (user>=13 ) {
+ 
+//     alert("bar-mitzva")
+// }else if (user==12) {
+    
+//     alert("bat-mitzvha")
+// }else{
+//     alert("kid")
+// }
+                     // תרגיל 2
+// var number = 0;
+// while (number<100) {
+    
+//        console.log("yes");
+//        number++  
+// } 
+                    // תרגיל 3
+// var number =0;
+// var avi =Number(prompt("your age"))
+// while (number<avi) {
+
+//     console.log("no");
+//     number++
+// }
+                    // תרגיל 4
+// for (var i = 0;i < 1000; i++) {
+    
+//     console.log("javascript is not java");
+    
+// }
+                         // תרגיל 5
+// var x =Number(prompt("enter your age"))
+
+
+// for (var i =0;i<x ;i++) {
+
+//     var y = prompt("enter your name")
+
+//     console.log(x);
+// }
+                        // תרגיל 6
+// var numOne =Number(prompt("enter age"))
+// var numTwo =Number(prompt("enter age"))
+// var numThree =Number(prompt("enter age"))
+// if(numOne<numTwo && numTwo<numThree) {
+//     console.log("סדר עולה ");
+// }else if(numOne>numTwo && numTwo>numThree) {
+//     console.log("סדר יורד");
+// }else{
+//     console.log("וואלה אין");
+// }
